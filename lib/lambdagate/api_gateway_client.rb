c0ff2a6aa1018cbed257ca9bef5d02be30c0263a
Ryo Nakamura <r7kamura@gmail.com>
2015-07-24 01:18:14 +0900

    Create signing request using aws4 gem

--- lib/lambdagate/api_gateway_client.rb
+++ lib/lambdagate/api_gateway_client.rb
@@ -1,3 +1,4 @@
+require "aws4"
 require "faraday"
 require "openssl"
 require "uri"
@@ -6,11 +7,38 @@ module Lambdagate
   class ApiGatewayClient
     DEFAULT_REGION = "us-east-1"
 
+    class << self
+      # @param [String] access_key
+      # @param [String] body
+      # @param [Hash] headers
+      # @param [String] region
+      # @param [String] request_method
+      # @param [String] secret_key
+      # @param [String] url
+      # @return [Hash]  Request headers that includes Authorization header
+      def sign(access_key:, body:, headers:, region:, request_method:, secret_key:, url:)
+        AWS4::Signer.new(
+          access_key: access_key,
+          secret_key: secret_key,
+          region: region,
+        ).sign(
+          request_method.upcase,
+          URI(url),
+          headers,
+          body,
+        )
+      end
+    end
+
+    # @param [String] access_key
     # @param [String, nil] host
     # @param [String, nil] region
-    def initialize(host: nil, region: nil)
+    # @param [String] secret_key
+    def initialize(access_key:, host: nil, region: nil, secret_key:)
+      @access_key = access_key
       @host = host
       @region = region
+      @secret_key = secret_key
     end
 
     # @param [String] path
@@ -70,43 +98,45 @@ module Lambdagate
       "apigateway.#{region}.amazonaws.com"
     end
 
+    # @return [Hash{String => String}]
+    def default_request_headers
+      {
+        "Date" => Time.now.iso8601,
+        "Host" => host,
+      }
+    end
+
     # @return [String]
     def host
       @host || default_host
     end
 
-    # @param [String] request_method
+    # @param [Symbol] request_method
     # @param [String] path
     # @param [Hash, nil] params
     # @param [Hash, nil] headers
     # @return [Faraday::Response]
     def process(request_method, path, params, headers)
+      headers = default_request_headers.merge(headers || {})
       connection.send(
         request_method,
         URI.escape(path),
         params,
-        headers,
+        self.class.sign(
+          access_key: @access_key,
+          body: "", # TODO
+          headers: headers,
+          region: region,
+          request_method: request_method.to_s,
+          secret_key: @secret_key,
+          url: "#{base_url}#{path}",
+        ),
       )
     end
 
     # @return [String]
     def region
       @region || DEFAULT_REGION
-    end
-
-    # @return [Hash{String => String}]
-    def request_headers
-      {
-        "Authorization" => signature,
-        "Host" => host,
-        "X-Amz-Date" => Time.now.iso8601,
-      }
-    end
-
-    # @todo
-    # @return [String]
-    def signature
-      "TODO"
     end
   end
 end
